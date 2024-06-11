228f2fcd3d4993300021bcfa9d5750e59f24d974
haiaza <ajames1342@gmail.com>
2024-06-11 19:44:48 -0400

    re-prettying my code

--- js/app.js
+++ js/app.js
@@ -2,73 +2,37 @@
 let guessDisplay = [];
 
 let board = [
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
-  "",
+    "","","","","",
+    "","","","","",
+    "","","","","",
+    "","","","","",
+    "","","","","",
+    "","","","","",
 ];
 // the user needs to get the correct guesses on the board in a line for a win.
 
 const winCondition = [
-  [0, 1, 2, 3, 4],
-  [5, 6, 7, 8, 9],
-  [10, 11, 12, 13, 14],
-  [15, 16, 17, 17, 19],
-  [20, 21, 22, 23, 24],
-  [25, 26, 27, 28, 29],
+    [0, 1, 2, 3, 4],
+    [5, 6, 7, 8, 9],
+    [10, 11, 12, 13, 14],
+    [15, 16, 17, 17, 19],
+    [20, 21, 22, 23, 24],
+    [25, 26, 27, 28, 29],
 ];
 
 const testWordBank = [
-  "About",
-  "Alert",
-  "Beach",
-  "Brief",
-  "Chart",
-  "Curve",
-  "Bring",
-  "Chase",
-  "Cycle",
-  "Broad",
-  "Cheap",
-  "Daily",
-  "Broke",
-  "Check",
-  "Dance",
+    "About","Alert","Beach","Brief","Chart",
+    "Curve","Bring","Chase","Cycle","Broad",
+    "Cheap","Daily","Broke","Check","Dance",
 ];
 
 /*-------------------------------- Functions --------------------------------*/
 //Upon loading, the game state should be initialized, and a function should be called to render this game state.
 
 function clickHandle(event) {
-  let target = event.target;
+    let target = event.target;
   // tagName returns all caps, so we search for allCaps.
-  if (target.tagName === "BUTTON") {
+    if (target.tagName === "BUTTON") {
     console.log(`We have clicked ${event.target.textContent}`);
     board[currentIndex] = target.textContent;
     let square = document.getElementById(currentIndex);
@@ -76,18 +40,18 @@ function clickHandle(event) {
     square.innerHTML = target.textContent;
 
     currentIndex++;
-  } else {
+    } else {
     alert("Click inside the button");
-  }
+    }
 
   //
 }
 
 function checkRow() {
   // each row is made of divs i need to target
-  const rowDivs = document.querySelectorAll("div.row div");
-  for (let div of rowDivs) {
-  }
+    const rowDivs = document.querySelectorAll("div.row div");
+    for (let div of rowDivs) {
+    }
 }
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -98,11 +62,11 @@ let currentGuess = "";
 let currentIndex = 0;
 squareIds = [];
 for (let i = 0; i < 29; i++) {
-  squareIds.push(document.getElementById(i));
+    squareIds.push(document.getElementById(i));
 }
 rowIds = [];
 for (let i = 0; i < 29; i++) {
-  rowIds.push(document.getElementById(i));
+    rowIds.push(document.getElementById(i));
 }
 let targetSquare = squareIds[currentIndex];
 
