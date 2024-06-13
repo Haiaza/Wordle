547eb21607441dc6f381ef868b4dc43fe6bbe1a5
haiaza <83097184+Haiaza@users.noreply.github.com>
2024-06-13 15:26:51 +0000

    ive added functionality to joinLetter once it reaches 5char but there is a bug

--- js/app.js
+++ js/app.js
@@ -2,102 +2,141 @@
 let guessDisplay = [];
 
 let board = [
-    "","","","","",
-    "","","","","",
-    "","","","","",
-    "","","","","",
-    "","","","","",
-    "","","","","",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
+  "",
 ];
 // the user needs to get the correct guesses on the board in a line for a win.
 
 const winCondition = [
-    [0, 1, 2, 3, 4],
-    [5, 6, 7, 8, 9],
-    [10, 11, 12, 13, 14],
-    [15, 16, 17, 17, 19],
-    [20, 21, 22, 23, 24],
-    [25, 26, 27, 28, 29],
+  [0, 1, 2, 3, 4],
+  [5, 6, 7, 8, 9],
+  [10, 11, 12, 13, 14],
+  [15, 16, 17, 17, 19],
+  [20, 21, 22, 23, 24],
+  [25, 26, 27, 28, 29],
 ];
 
 const testWordBank = [
-    "About","Alert","Beach","Brief","Chart",
-    "Curve","Bring","Chase","Cycle","Broad",
-    "Cheap","Daily","Broke","Check","Dance",
+  "About",
+  "Alert",
+  "Beach",
+  "Brief",
+  "Chart",
+  "Curve",
+  "Bring",
+  "Chase",
+  "Cycle",
+  "Broad",
+  "Cheap",
+  "Daily",
+  "Broke",
+  "Check",
+  "Dance",
 ];
 
 /*-------------------------------- Functions --------------------------------*/
 //Upon loading, the game state should be initialized, and a function should be called to render this game state.
 
 function clickHandle(event) {
-    let target = event.target;
+  let target = event.target;
   // tagName returns all caps, so we search for allCaps.
-    if (target.tagName === "BUTTON") {
+  if (target.tagName === "BUTTON") {
     board[currentIndex] = target.textContent;
     let square = document.getElementById(currentIndex);
     // since the id's of each box is just their index, we can use that to get the square.
     square.innerHTML = target.textContent;
-    letterList.push(target.textContent)
-    
-    checkRow()
-    joinLetter()
+    letterList.push(target.textContent);
+
+    checkRow();
+    joinLetter();
     currentIndex++;
-    } else {
+  } else {
     alert("Click inside the button");
-    }
-    
+  }
+
   //
 }
 
 function checkRow(rowNumber) {
   // each row is made of divs i need to target
-    const rowDivs = document.querySelectorAll(`#row${rowNumber} div`);
-    for (let div of rowDivs) {
-        if (div.textContent === '') {
-            console.log('Test failed');
-            return false
-        }
-        //todo code for the correctGuess
-    } return true
+  const rowDivs = document.querySelectorAll(`#row${rowNumber} div`);
+  for (let div of rowDivs) {
+    if (div.textContent === "") {
+      console.log("Test failed");
+      return false;
+    }
+    //todo code for the correctGuess
+  }
+  return true;
 }
 
-function joinLetter(){
-    if (currentGuess.length < 4) {
-        console.log(`${currentGuess.length + 1} letters`)
-    } currentGuess = letterList.join('');
-    if (currentGuess.length = 5) {
-        // somewhere i need to work in a string split that makes the ending portion lowercase.
-    
-    console.log(currentGuess)
-}
+function joinLetter() {
+  if (currentGuess.length < 4) {
+    console.log(`${currentGuess.length + 1} letters`);
+  }
+  currentGuess = letterList.join("");
+  if (currentGuess.length === 5) {
+    // somewhere i need to work in a string split that makes the ending portion lowercase.
+    let p1 = currentGuess.slice(0, 1);
+    let p2 = currentGuess.slice(1);
+    let rebuild = p1.toUpperCase() + p2.toLowerCase();
+    rebuild = currentGuess; // the logic looks good but there is a bug
+    console.log(`${currentGuess} New Message`);
+  }
 
-
-function correctGuess(){
+  function correctGuess() {
     if (currentGuess === randomWord) {
-        console.log('Match found')
+      console.log("Match found");
     } else {
-        alert('Nice try')
+      alert("Nice try");
     }
+  }
+  // make function for checking if the two match. if the return is true...tbd
+  // if the return is false current guess will be reverted back to being blank
 }
-// make function for checking if the two match. if the return is true...tbd
-        // if the return is false current guess will be reverted back to being blank
-    }
 /*---------------------------- Variables (state) ----------------------------*/
 let victory = false;
 let defeat = false;
 //  pushed into every click. check if this array content matches the randomWord
-let letterList = []
+let letterList = [];
 let currentGuess = "";
 let currentIndex = 0;
 squareIds = [];
 for (let i = 0; i < 29; i++) {
-    squareIds.push(document.getElementById(i));
-} 
+  squareIds.push(document.getElementById(i));
+}
 
-
-
-const randomWord = testWordBank[Math.floor(Math.random()*testWordBank.length)]
-// i googled 'how to randomize the selection from an array' NaN was due to the random number being put against literally Not a Number. i relearned .length functions as a counter for array items. 
+const randomWord =
+  testWordBank[Math.floor(Math.random() * testWordBank.length)];
+// i googled 'how to randomize the selection from an array' NaN was due to the random number being put against literally Not a Number. i relearned .length functions as a counter for array items.
 /*------------------------ Cached Element References ------------------------*/
 const buttons = document.querySelectorAll("button");
 const Squares = document.querySelectorAll(".square");
@@ -113,9 +152,6 @@ keyboard.addEventListener("click", clickHandle);
 // event delegation ??
 /*----------------------------- Code -----------------------------*/
 
-
-
-
 /*----------------------------- Code Graveyard -----------------------------*/
 
 // rowIds = [];
@@ -123,5 +159,5 @@ keyboard.addEventListener("click", clickHandle);
 //     rowIds.push(document.getElementById(i));
 // }  Im not sure i will need this code anymore but it may be useful
 
-// let targetSquare = squareIds[currentIndex]; 
+// let targetSquare = squareIds[currentIndex];
 // i had a good idea with this but im not at that stage yet
